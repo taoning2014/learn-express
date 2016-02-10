6dd81fb7bd2ab8c1abbe1c3fbb5dc3cca0a90045
Ning, Tao <tao.ning@priceline.com>
2016-02-10 17:14:42 -0500

    add serve static files

--- meadowlark.js
+++ meadowlark.js
@@ -9,7 +9,9 @@ var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
+app.use(express.static(__dirname + '/public'));
+
+app.get('/home', function(req, res) {
   res.render('home');
 })
 
@@ -19,7 +21,7 @@ app.get('/about', function(req, res) {
 
 
 app.use(function(req, res) {
-  res.render('home');
+  res.render('404');
 });
 
 app.listen(app.get('port'), function() {
