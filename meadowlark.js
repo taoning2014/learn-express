6d8e8f715c0bc65458f649b3cc888f3b21b09ad4
Ning, Tao <tao.ning@priceline.com>
2016-02-10 17:11:19 -0500

    add view engine

--- meadowlark.js
+++ meadowlark.js
@@ -4,21 +4,22 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 })
 
 app.get('/about', function(req, res) {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  res.render('about');
 })
 
 
 app.use(function(req, res) {
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
+  res.render('home');
 });
 
 app.listen(app.get('port'), function() {
