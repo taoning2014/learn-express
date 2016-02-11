d6aa1cb05407008af23d206901886254a42e83ab
Ning, Tao <tao.ning@priceline.com>
2016-02-10 19:13:52 -0500

    add global test

--- meadowlark.js
+++ meadowlark.js
@@ -5,13 +5,19 @@ var fortuneCookie = require('./lib/fortune');
 
 app.set('port', process.env.PORT || 3000);
 
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
+app.use(function(req, res, next) {
+  res.locals.showTests = (req.query.test == 1);
+  next();
+})
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/home', function(req, res) {
+  console.log('res.locals.showTests');
+  console.log(res.locals.showTests);
   res.render('home');
 })
 
